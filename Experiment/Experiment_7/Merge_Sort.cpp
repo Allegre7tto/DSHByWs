33e502ad7e14647de5364d7f773e3a33f351a081
allegre7tto <allegre7tto@qq.com>
2024-01-15 21:55:54 +0800

    update20240115

--- Experiment/Experiment_7/Merge_Sort.cpp
+++ Experiment/Experiment_7/Merge_Sort.cpp
@@ -9,6 +9,7 @@ const int n = 5*1e4;
 
 void Merge(int arr[],int res[],int left,int mid,int right)
 {
+
     int i = left,j = mid+1,k = left;
     while (i<=mid && j<=right){
         if (arr[i]<=arr[j])
@@ -20,21 +21,24 @@ void Merge(int arr[],int res[],int left,int mid,int right)
         res[k++] = arr[i++];
     while (j<=right)
         res[k++] = arr[j++];
+    for (int i=left;i<=right;i++)
+        arr[i] = res[i];
 }
 
-void MergeSort(int arr[],int res[],int left,int right)
+void MergeSort(int arr[],int left,int right)
 {
+    static int tmp[n]={}; // int tmp[right-left+1
     if (left>right) 
         return;
     if (left==right) {
-        res[left] = arr[left];
+        tmp[left] = arr[left];
         return;
     }
-    int tmp[n]; // int tmp[right-left+1
+   
     int mid = (left+right)/2;
-    MergeSort(arr,tmp,left,mid);
-    MergeSort(arr,tmp,mid+1,right);
-    Merge(tmp,res,left,mid,right);
+    MergeSort(arr,left,mid);
+    MergeSort(arr,mid+1,right);
+    Merge(arr,tmp,left,mid,right);
 }
 
 int main()
@@ -46,11 +50,11 @@ int main()
         arr[i] = rand()%1000;
     /*for (int i=0;i<n;i++) 
         cout << arr[i] << " "; */
-    int res[n];
+    //int res[n];
     cout <<endl;
-    MergeSort(arr,res,0,n-1);
+    MergeSort(arr,0,n-1);
     for(int i=0;i<n;i+=200) 
-        cout << res[i] << " ";
+        cout << arr[i] << " ";
 
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double> diff = end-start;
